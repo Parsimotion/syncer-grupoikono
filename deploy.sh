c107f179e517ae5c03068706aa91fd98f4f4e7cf
Andres Kirschbaum <andres@parsimotion.com>
2015-10-25 11:27:52 -0300

    vuelvo a como estaba

--- deploy.sh
+++ deploy.sh
@@ -110,14 +110,17 @@ fi
 selectNodeVersion
 
 # 3. Install npm packages
-npm config set strict-ssl false
-npm install -g node-gyp@latest
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install --msvs_version=2012 --production
+  eval $NPM_CMD install --production
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
+
+# 4. Copy pre-compiled node modules
+echo Copying pre-compiled node modules
+mkdir -p node_modules/xml-stream
+cp -rf pre_compiled_modules/xml-stream/ node_modules/xml-stream
 
 ##################################################################################################################################
 
