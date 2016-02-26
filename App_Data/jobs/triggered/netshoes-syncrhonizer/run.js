268504c4d061a659f93c9774189d5fcbc1002444
andreskir <andres@parsimotion.com>
2016-02-26 15:54:50 -0300

    chau netshoes y woow

--- App_Data/jobs/triggered/netshoes-syncrhonizer/run.js
+++
@@ -1,5 +0,0 @@
-require("coffee-script/register");
-
-var synchronizer = require("../synchronizer.coffee");
-
-synchronizer.synchronize(process.env.NETSHOES_USER_ID);
--- App_Data/jobs/triggered/woow-syncrhonizer/run.js
+++
@@ -1,5 +0,0 @@
-require("coffee-script/register");
-
-var synchronizer = require("../synchronizer.coffee");
-
-synchronizer.synchronize(process.env.WOOW_USER_ID);
